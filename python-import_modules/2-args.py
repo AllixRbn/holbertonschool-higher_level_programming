47c24e7fdf7bb8279089f2e2c468a7f7acbf7609
Allix <thewitchonxanax@gmail.com>
2026-01-16 09:42:59 +0000

    fix: Correct argument count checks in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     argc = len(args)
 
-    if args == 0:
+    if argc == 0:
         print("0 arguments.")
-    elif args == 1:
+    elif argc == 1:
         print("1 argument:")
     else:
-        print("{} argument:".format(argc))
+        print("{} arguments:".format(argc))
 
     for i, argument in enumerate(args, start=1):
         print("{}: {}".format(i, argument))
