afa48eccdae11f8fe24d36f7dce94cc264d11020
Allix <thewitchonxanax@gmail.com>
2026-01-16 09:32:54 +0000

    fix: Correct argument count handling and output formatting in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,14 +5,14 @@ import sys
 if __name__ == "__main__":
 
     args = sys.argv[1:]
-    i = len(args)
+    argc = len(args)
 
     if args == 0:
-        print("0 arguments:")
+        print("0 arguments.")
     elif args == 1:
         print("1 argument:")
     else:
-        print("{} argument:".format(i))
+        print("{} argument:".format(argc))
 
     for i, argument in enumerate(args, start=1):
         print("{}: {}".format(i, argument))
