b7890a1c30b1fa410bf465fe0a20b80fb78afde6
Allix <thewitchonxanax@gmail.com>
2026-01-19 14:12:48 +0000

    fix: Update condition checks in add_tuple to handle tuples with more than two elements

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a1 = 0
     if len(tuple_a) == 1:
         a0 = tuple_a[0]
-    elif len(tuple_a) == 2:
+    elif len(tuple_a) >= 2:
         a0 = tuple_a[0]
         a1 = tuple_a[1]
 
@@ -14,7 +14,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b1 = 0
     if len(tuple_b) == 1:
         b0 = tuple_b[0]
-    elif len(tuple_b) == 2:
+    elif len(tuple_b) >= 2:
         b0 = tuple_b[0]
         b1 = tuple_b[1]
 
