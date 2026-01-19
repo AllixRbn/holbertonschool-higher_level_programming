ed53e183552caaf9908b4ef8f9d76025b3da4e5f
Allix <thewitchonxanax@gmail.com>
2026-01-19 15:16:12 +0000

    fix: Move maximum initialization to after empty list check in max_integer function

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    maximum = my_list[0]
+
     if len(my_list) == 0:
         return None
     else:
+        maximum = my_list[0]
         for i in range(len(my_list)):
             if maximum < my_list[i]:
                 maximum = my_list[i]
