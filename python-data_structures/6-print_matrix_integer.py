a46aecee12936cff51afa2639b25642c6c4dda4c
Allix <thewitchonxanax@gmail.com>
2026-01-19 13:08:23 +0000

    fix: Correct format specifier in print_matrix_integer to ensure integers are printed correctly

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for idx in range(len(matrix)):
         for idx2 in range(len(matrix[idx])):
             if idx2 == len(matrix[idx]) - 1:
-                print("{}".format(matrix[idx][idx2]), end="")
+                print("{:d}".format(matrix[idx][idx2]), end="")
             else:
-                print("{}".format(matrix[idx][idx2]), end=" ")
+                print("{:d}".format(matrix[idx][idx2]), end=" ")
         print("")
