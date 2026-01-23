671bd7720d0a26b12691d7aa74ee099daf4b946d
Allix <thewitchonxanax@gmail.com>
2026-01-23 12:48:07 +0000

    feat: Implement safe_print_division function to safely divide two integers

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        if isinstance(a, int) and isinstance(b, int):
+            result = a / b
+            return result
+    except ZeroDivisionError:
+        result = None
+        return result
+    finally:
+        print("Inside result: {}".format(result))
