076701eec034f4ee2a3ed60895dc5bf5abef23d9
Allix <thewitchonxanax@gmail.com>
2026-01-23 15:16:09 +0000

    feat: Implement add_integer function for adding two integers with type checks

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    Addition of two integers
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+    result = a + b
+    return result
