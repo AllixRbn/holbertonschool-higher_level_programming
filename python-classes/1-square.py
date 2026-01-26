6b766aaeda72eaffabbe0088d2df14a780787db5
Allix <thewitchonxanax@gmail.com>
2026-01-26 13:54:55 +0000

    feat: Implement Square class with size attribute and docstring

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Defines a square
+
+    args:
+    - size: defines the size of the square
+    """
+    def __init__(self, size=0):
+        self.__size = size
