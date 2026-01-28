dc063d92e4f6e03ff1cc7a635d7329346a3707b9
Allix <thewitchonxanax@gmail.com>
2026-01-28 10:11:34 +0000

    fix: Enhance class docstring for Square to provide detailed explanations

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
+"""
+Class Square that defines a square by:
+
+Private instance attribute: size
+
+Instantiation with optional size: def __init__(self, size=0):
+
+If size is not an integer raise a TypeError exception with the message
+size must be an integer
+
+If size is less than 0, raise a ValueError exception with the message
+size must be >= 0
+
+Public instance method: def area(self):
+returns the current square area
+
+"""
+
 
 class Square:
     """
