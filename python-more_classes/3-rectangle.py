4ab1883a7408beb237618dfaaa3fb3f357b798d5
Allix <thewitchonxanax@gmail.com>
2026-01-29 15:08:10 +0000

    fix: Update string representation of Rectangle class to avoid extra newline at the end

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -80,8 +80,9 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        else:
-            rectangle = ""
-            for i in range(self.__height):
-                rectangle += ("#" * self.__width) + "\n"
-            return rectangle
+        rectangle = ""
+        for i in range(self.__height):
+            rectangle += ("#" * self.__width) + "\n"
+            if i == self.__height - 1:
+                rectangle += "#" * self.__width
+        return rectangle
