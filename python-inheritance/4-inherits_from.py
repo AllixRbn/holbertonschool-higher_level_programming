459c7cdf1baf6f1318de32695e8a4c76f447b50f
Allix <thewitchonxanax@gmail.com>
2026-02-04 10:12:44 +0000

    fix: Correct docstring formatting in inherits_from function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """
 Defines a function that returns True if the object is an instance
 of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
+the specified class ; otherwise False.
 """
 
 
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
     """
     Returns True if the object is an instance
     of a class that inherited (directly or indirectly)
-    from the specified class ; otherwise False.
+    the specified class ; otherwise False.
     """
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
