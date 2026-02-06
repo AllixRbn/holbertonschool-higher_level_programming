1552bf53cf2941c6c0f1862a0f4b6db03e85328e
Allix <thewitchonxanax@gmail.com>
2026-02-06 13:38:04 +0000

    feat: Implement BaseGeometry class with area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry
+    """
+
+    def area(self):
+        """
+        Raises an exception ebcause area is not implemented
+        """
+        raise Exception("area() is not implemented")
