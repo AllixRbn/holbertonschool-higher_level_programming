ba69d7f75ac51be485727216ba04b70a615e6b48
Allix <thewitchonxanax@gmail.com>
2026-02-12 15:57:30 +0000

    fix: Ensure print output does not add extra newline in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     Reads a text file and prints its content
     """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
