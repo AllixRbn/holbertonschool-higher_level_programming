2abf83bb9abdb482883a326406a40eec1e12eea2
Allix <thewitchonxanax@gmail.com>
2026-02-12 15:53:26 +0000

    feat: Add 0-read_file.py to define a function for reading and printing file content

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a function to read a text file and print its content
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints its content
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
