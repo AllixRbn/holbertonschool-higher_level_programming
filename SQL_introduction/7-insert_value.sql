7e511abf6f20c7d27e25e097ad284551c47a0f20
Allix <thewitchonxanax@gmail.com>
2026-03-27 23:12:29 +0000

    feat: Add SQL script to insert a new record into the first_table

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Insert a new record into the `first_table` table with the following values:
+-- - `id`: 89
+-- - `name`: 'Best School'
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
