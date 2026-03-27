55a9c2ed995166f22789e6151701624600275833
Allix <thewitchonxanax@gmail.com>
2026-03-27 23:18:13 +0000

    fix: Correct SQL query to count records in first_table by using COUNT(id) instead of COUNT(*)

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 --Counts the number of records in the `first_table` table where the `id` is equal to 89
-SELECT COUNT(*) FROM first_table WHERE id = 89;
+SELECT COUNT(id) FROM first_table WHERE id = 89;
