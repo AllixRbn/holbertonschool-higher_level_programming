149dfcb66c882515884099faea4fad413428b448
Allix <thewitchonxanax@gmail.com>
2026-03-27 23:02:55 +0000

    feat: Create SQL script to define the first_table with id and name columns

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Create a table named `first_table` with the following columns:
+-- - `id`: an integer
+-- - `name`: a variable character string with a max length of 255 characters
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(255)
+);
