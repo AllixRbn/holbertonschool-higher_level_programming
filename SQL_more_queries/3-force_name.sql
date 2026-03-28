1133e9c83ac79b41cb5d92ac944627463fc6d6a2
Allix <thewitchonxanax@gmail.com>
2026-03-28 00:51:45 +0000

    feat: Create SQL script to define force_name table with id and name columns

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Creates the table force_name in the database hbtn_0d_2 with the following columns:
+-- id: INT
+-- name: VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
